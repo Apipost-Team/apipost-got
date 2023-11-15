e546507eda5057a5fc7072c801c56dcbeae6b5ea
phpwechat <3257132998@qq.com>
2023-11-15 23:57:06 +0800

    init

--- source/core/utils/is-form-data.ts
+++
@@ -1,9 +0,0 @@
-import is from '@sindresorhus/is';
-import {Readable} from 'stream';
-
-interface FormData extends Readable {
-	getBoundary: () => string;
-	getLength: (callback: (error: Error | null, length: number) => void) => void;
-}
-
-export default (body: unknown): body is FormData => is.nodeStream(body) && is.function_((body as FormData).getBoundary);
